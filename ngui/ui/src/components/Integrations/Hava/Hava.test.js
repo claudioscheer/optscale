ad5df78f5a471a5ab13af765ce2dcc11e5a83066
Claudio Scheer <claudioscheer@protonmail.com>
2023-10-13 14:41:21 -0300

    test: unit test hava

--- ngui/ui/src/components/Integrations/Hava/Hava.test.js
+++ ngui/ui/src/components/Integrations/Hava/Hava.test.js
@@ -1,15 +1,15 @@
-// import React from "react";
-// import { createRoot } from "react-dom/client";
-// import TestProvider from "tests/TestProvider";
-// import Slack from "./Slack";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import TestProvider from "tests/TestProvider";
+import Hava from "./Hava";
 
-// it("renders without crashing", () => {
-//   const div = document.createElement("div");
-//   const root = createRoot(div);
-//   root.render(
-//     <TestProvider>
-//       <Slack />
-//     </TestProvider>
-//   );
-//   root.unmount();
-// });
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  const root = createRoot(div);
+  root.render(
+    <TestProvider>
+      <Hava />
+    </TestProvider>
+  );
+  root.unmount();
+});
