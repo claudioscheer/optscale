d81ef04c7a8d5840a9463fc2a916a9bd240dc109
Claudio Scheer <claudioscheer@protonmail.com>
2023-10-25 21:32:06 -0300

    chore: migration to create haava integration table

---
+++ rest_api/rest_api_server/alembic/versions/ef5ed8b5bed4_create_hava_integration_table.py
@@ -0,0 +1,35 @@
+"""create hava integration table
+
+Revision ID: ef5ed8b5bed4
+Revises: 3273906c73ac
+Create Date: 2023-10-25 21:26:08.452719
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ef5ed8b5bed4'
+down_revision = '3273906c73ac'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        "hava_integration",
+        sa.Column("deleted_at", sa.Integer(), nullable=False),
+        sa.Column("organization_id", sa.String(length=36), nullable=False),
+        sa.Column("created_at", sa.Integer(), nullable=False),
+        sa.Column("hava_api_key", sa.TEXT(), nullable=True),
+        sa.Column("enabled", sa.Boolean(), nullable=False, default=False),
+
+        sa.PrimaryKeyConstraint("organization_id"),
+        sa.ForeignKeyConstraint(["organization_id"], ["organization.id"], )
+    )
+    pass
+
+
+def downgrade():
+    op.drop_table("hava_integration")
--- rest_api/rest_api_server/controllers/context.py
+++ rest_api/rest_api_server/controllers/context.py
@@ -5,7 +5,7 @@ from rest_api.rest_api_server.controllers.base import (
     BaseHierarchicalController, MongoMixin)
 from rest_api.rest_api_server.exceptions import Err
 from rest_api.rest_api_server.models.models import (
-    Organization, CloudAccount, Employee, Pool, ReportImport, PoolAlert,
+    HavaIntegration, Organization, CloudAccount, Employee, Pool, ReportImport, PoolAlert,
     PoolPolicy, ResourceConstraint, Rule, ShareableBooking, Webhook,
     OrganizationConstraint, OrganizationBI, OrganizationGemini)
 from tools.optscale_exceptions.common_exc import (WrongArgumentsException,
@@ -45,7 +45,7 @@ class ContextController(MongoMixin):
                              'resource_constraint', 'rule',
                              'shareable_booking', 'webhook',
                              'organization_constraint', 'organization_bi',
-                             'organization_gemini']:
+                             'organization_gemini', 'hava_integration']:
             raise WrongArgumentsException(Err.OE0174, [type_name])
         return type_name, uuid
 
@@ -63,6 +63,7 @@ class ContextController(MongoMixin):
             'organization_constraint': OrganizationConstraint.__name__,
             'organization_bi': OrganizationBI.__name__,
             'organization_gemini': OrganizationGemini.__name__,
+            'hava_integration': HavaIntegration.__name__,
         }
 
         def call_query(base):
@@ -87,7 +88,8 @@ class ContextController(MongoMixin):
             'organization_constraint': (self.session.query(OrganizationConstraint),
                                         call_query),
             'organization_bi': (self.session.query(OrganizationBI), call_query),
-            'organization_gemini': (self.session.query(OrganizationGemini), call_query)
+            'organization_gemini': (self.session.query(OrganizationGemini), call_query),
+            'hava_integration': (self.session.query(HavaIntegration), call_query),
         }
 
         query_base, func = query_map.get(type_name)
